a91cc33894d1f51c68a8a829543124f19feea5a2
Seunghyun Lee <shlee0605@gmail.com>
2014-12-04 23:59:24 +0900

    updated visualize.py

--- script/visualize.py
+++ script/visualize.py
@@ -13,23 +13,23 @@ Author: Seunghyun Lee (sl169@duke.edu)
 def main(argv):
     """
     main function takes care of the input arguments
-    """    
+    """
     if len(argv) < 2:
-        print 'visualize.py -i <inputfile> -r <resultfile> -o <outputfile>'
+        print 'visualize.py -i <initial graph input data> -r <result file after k-th round> -o <outputfile>'
         exit(1)
     argv = argv[1:]
-    
+
     inputfile = ''      # initial graph input data
     resultfile = ''     # intermediate result file after k-th round
     outputfile = ''     # output Json file
     try:
         opts, args = getopt.getopt(argv,"hi:o:r:",["ifile=","ofile=", "rfile="])
     except getopt.GetoptError:
-        print 'visualize.py -i <inputfile> -r <resultfile> -o <outputfile>'
+        print 'visualize.py -i <initial graph input data> -r <result file after k-th round> -o <outputfile>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'visualize.py -i <inputfile> -r <resultfile> -o <outputfile>'
+            print 'visualize.py -i <initial graph input data> -r <result file after k-th round> -o <outputfile>'
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
@@ -44,10 +44,11 @@ def parse(inputfile, resultfile, outputfile):
     result = {}
     nodes = []
     links = []
-    
+
     # read in initial input graph and get the nodes, edges
     for line in file:
-        data = line.split("\t")
+        data = line.split()
+        print data[0]
         data_0 = int(data[0])
         data_1 = int(data[1])
         if data_0 not in nodes:
@@ -56,11 +57,11 @@ def parse(inputfile, resultfile, outputfile):
             nodes.append(data_1)
         if(data[0] != data[1]):
             links.append(data)
-    
+
     # convert the results into the json format
     nodes_result = []
     links_result = []
-        
+
     if resultfile == '':
         for node in sorted(nodes):
             nodes_result.append({"name": str(node), "group": node})
@@ -68,14 +69,14 @@ def parse(inputfile, resultfile, outputfile):
         nodes_result = parse_round(resultfile)
     for link in links:
         links_result.append({"source": int(link[0]), "target":int(link[1]), "value" : 1})
-    
+
     result["nodes"] = nodes_result
     result["links"] = links_result
 
     final = json.dumps(result)
     output = open(outputfile, 'w')
     output.write(final)
-    
+
 def parse_round(input):
     file = open(input, 'r')
     nodes=[]
@@ -83,6 +84,6 @@ def parse_round(input):
         data = re.split(r'[(,)]', line)
         nodes.append({"name": data[1], "group":data[3]})
     return nodes
-    
+
 if __name__ == '__main__':
     main(sys.argv)
